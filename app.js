8ea3346da56e915773f6b0f662099520f6432994
Valentin Bonnamour <valentinbonnamour@gmail.com>
2020-09-16 15:40:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//Déclaration de variable
+let button = document.getElementById('nouveau');
+let citationDisplay = document.getElementById('citation');
+let author = document.getElementById('auteur');
+
+
+let dernier = 0;
+let nombreAleatoire = 0;
+
+// La fonction prend en paramètre max, qui est la taille du tableau "citations" et on va renvoyer un nombre aleatoire en entier grâce à la fonction Math.floor .
+function genererNombreEntier(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+//Évents 
+button.addEventListener('click', () => {
+    do {
+        nombreAleatoire = genererNombreEntier(citations.length)
+    } while (nombreAleatoire == dernier);
+
+    citationDisplay.textContent = citations[nombreAleatoire][0];
+    author.textContent = citations[nombreAleatoire][1];
+    dernier = nombreAleatoire;
+});
+
+
+// Tableau de citations
+let citations = [
+    ["La vie est un mystère qu'il faut vivre, et non un problème à résoudre.", "Gandhi"],
+    ["Le plus grand risque est de ne prendre aucun risque.", "Mark Zuckerberg"],
+    ["Méritez votre statut de leader chaque jour.", "Mickael Jordan"],
+    ["Soyez le changement que vous voulez voir dans le monde.", "Gandhi"],
+    ["A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.", "Mark Twain"],
+    ["Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.", "T.S Elliot"],
+    ["Le succès c’est tomber sept fois, se relever huit.", "Proverbe japonais"],
+    ["Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.", "Mark Twain"],
+    ["Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.", "Brian Tracy"],
+    ["Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.", "P.T. Barnum"],
+    ["Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.", "Brian Sher"],
+    ["Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.", "Anita Roddick"],
+    ["Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.", "Robert Louis Stevenson"],
+    ["L’action est la clé fondamentale de tout succès.", "Pablo Picasso"],
+    ["Le succès, c’est se promener d’échecs en échecs tout en restant motivé.", "Winston Churchill"],
+    ["Votre avenir est créé par ce que vous faîtes aujourd’hui, pas demain.", "Robert T. Kiyosaki"],
+    ["Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.", "Zig Ziglar"],
+    ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
+    ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
+    ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
+    ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"],
+    ["Nos connaissances sont une goutte, notre ignorance, un océan.", "Isaac Newton"],
+    ["La vie, c'est comme une bicyclette, il faut avancer pour ne pas perdre l'équilibre.","Albert Einstein"],
+    ["N'essayez pas de devenir un homme qui a du succès. Essayez de devenir un homme qui a de la valeur.","Albert Einstein"],
+    ["La technique résout les problèmes et apporte des satisfactions mais elle n’est qu’un moyen et reste pauvre si on la sépare de l’esprit qui la guide.","Gaston Rébuffat"],
+    ["La vie, en définitive, n'a de sens que si on la vit en se donnant à fond, en cherchant à réaliser tout ce qu'on a en soi.","Walter Bonatti"]
+  ];
+
